b84e0bacafe63b76ad7c0055236d9d52cec0bbda
Matteo Pagani <matteo.pagani@microsoft.com>
2022-05-19 17:59:23 +0200

    first commit

---
+++ MyComponent/SampleControl.xaml.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include "SampleControl.xaml.h"
+#if __has_include("SampleControl.g.cpp")
+#include "SampleControl.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::MyComponent::implementation
+{
+    SampleControl::SampleControl()
+    {
+        InitializeComponent();
+    }
+
+    int32_t SampleControl::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void SampleControl::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void SampleControl::myButton_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        myButton().Content(box_value(L"Clicked"));
+    }
+}
---
+++ MyComponent/SampleControl.xaml.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "winrt/Microsoft.UI.Xaml.h"
+#include "winrt/Microsoft.UI.Xaml.Markup.h"
+#include "winrt/Microsoft.UI.Xaml.Controls.Primitives.h"
+#include "SampleControl.g.h"
+
+namespace winrt::MyComponent::implementation
+{
+    struct SampleControl : SampleControlT<SampleControl>
+    {
+        SampleControl();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::MyComponent::factory_implementation
+{
+    struct SampleControl : SampleControlT<SampleControl, implementation::SampleControl>
+    {
+    };
+}
